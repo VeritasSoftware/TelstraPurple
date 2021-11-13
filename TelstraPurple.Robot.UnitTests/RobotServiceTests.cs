f4a092c2a39f0eef2328f1fccb0b2cd1985fb054
Shantanu <shantanu_negi@hotmail.com>
2021-11-13 17:04:54 +1100

    added unit tests.

---
+++ TelstraPurple.Robot.UnitTests/RobotServiceTests.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Linq;
+using TelstraPurple.Robot.UI.Models;
+using TelstraPurple.Robot.UI.Services;
+using Xunit;
+
+namespace TelstraPurple.Robot.UnitTests
+{
+    public class RobotServiceTests
+    {
+        [Fact]
+        public void Commands_Parse_Success()
+        {
+            var robotService = new RobotService();
+
+            var strCommands = @"
+                PLACE 1,2,EAST
+                MOVE
+                MOVE
+                LEFT
+                MOVE
+            ";
+
+            var commands = robotService.ParseCommands(strCommands);
+
+            Assert.Equal(CommandType.PLACE, commands.ElementAt(0).Name);
+            Assert.Equal(CommandType.MOVE, commands.ElementAt(1).Name);
+            Assert.Equal(CommandType.MOVE, commands.ElementAt(2).Name);
+            Assert.Equal(CommandType.LEFT, commands.ElementAt(3).Name);
+            Assert.Equal(CommandType.MOVE, commands.ElementAt(4).Name);
+
+            strCommands = @"
+                PLACE 1,2,EAST
+                MOVE
+                LEFT
+                MOVE
+                PLACE 3,1
+                MOVE
+            ";
+
+            commands = robotService.ParseCommands(strCommands);
+
+            Assert.Equal(CommandType.PLACE, commands.ElementAt(0).Name);
+            Assert.Equal(CommandType.MOVE, commands.ElementAt(1).Name);
+            Assert.Equal(CommandType.LEFT, commands.ElementAt(2).Name);
+            Assert.Equal(CommandType.MOVE, commands.ElementAt(3).Name);
+            Assert.Equal(CommandType.PLACE, commands.ElementAt(4).Name);
+            Assert.Equal(CommandType.MOVE, commands.ElementAt(5).Name);
+        }
+    }
+}
